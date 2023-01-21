172370fa62bd0cdccca6748385b6380e4525dc38
m.mirdar <mohsen.citydi@gmail.com>
2023-01-21 17:52:20 +0330

    added toggle favourite method in view model

--- app/src/main/java/com/example/restaurantapp/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantapp/RestaurantViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.restaurantapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class RestaurantViewModel() : ViewModel() {
 
+    private val state = mutableStateOf(dummyRestaurants)
+
+
     fun getRestaurants() = dummyRestaurants
+
+    fun toggleFavourite(id: Int) {
+        val restaurants = state.value.toMutableList()
+        val itemIndex = restaurants.indexOfFirst { it.id == id }
+
+        val item = restaurants[itemIndex]
+
+        restaurants[itemIndex] = item.copy(isFavourite = !item.isFavourite)
+        state.value = restaurants
+    }
 
 }
